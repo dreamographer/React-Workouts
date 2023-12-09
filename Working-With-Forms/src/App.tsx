c039d952cdeff3222f205779af93bfcb1b8aa5ec
Ashwin <ashwinkv.akv@gmail.com>
2023-12-09 11:48:02 +0530

    final

---
+++ Working-With-Forms/src/App.tsx
@@ -0,0 +1,11 @@
+// import Form from "./components/Form"
+import Form3 from "./components/Form3"
+
+function App(){
+  return(
+  <div className="bg-black w-screen h-screen items-center">
+    <Form3></Form3>
+  </div>
+  )
+}
+export default App         
